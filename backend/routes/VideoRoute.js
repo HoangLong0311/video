0b0662820a63a570a76d326600e25af5576494e4
VuManh <vuducmanh10a@gmail.com>
2024-12-19 19:28:40 +0700

    updated apis

--- backend/routes/VideoRoute.js
+++ backend/routes/VideoRoute.js
@@ -4,5 +4,10 @@ const videoController = require('../controllers/VideoController');
 videoApi.get('/video-list', videoController.getVideoList);
 
 videoApi.get('/video-html', videoController.getVideoHtml);
+videoApi.post('/add-video', videoController.addVideo);
+
+videoApi.delete('/delete-video', videoController.deleteVideo);
+videoApi.get('/video-list-all', videoController.getVideoListAll);
+
 
 module.exports = videoApi;
--- backend/services/VideoService.js
+++ backend/services/VideoService.js
@@ -9,6 +9,16 @@ exports.getVideoListService = async () => {
   }
 };
 
+exports.getVideoListServiceAll = async () => {
+  try {
+    const videos = await VideoModel.find({}).select('-html');
+    return videos;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
 exports.getVideoHtmlService = async (_id) => {
   try {
     if (!Boolean(_id)) return null;
@@ -16,3 +26,24 @@ exports.getVideoHtmlService = async (_id) => {
     return html;
   } catch (error) {}
 };
+
+exports.addVideoService = async (videoData) => {
+  try {
+    const newVideo = new VideoModel(videoData);
+    await newVideo.save();
+    return newVideo;
+  } catch (error) {
+    throw error;
+  }
+};
+
+exports.deleteVideoService = async (_id) => {
+  try {
+    if (!Boolean(_id)) return null;
+    const deletedVideo = await VideoModel.findByIdAndDelete(_id);
+    return deletedVideo;
+  } catch (error) {
+    throw error;
+  }
+};
+
